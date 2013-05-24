f43b199128dbf92282e3397d07a3ad0834de0219
Ghislain Loaec <gloaec@cadoles.com>
2013-05-24 20:11:25 +0200

    PerlDoc for PerlEcommerce

--- lib/PerlEcommerce.pm
+++ lib/PerlEcommerce.pm
@@ -72,30 +72,6 @@ sub setup_routing {
   $rrrr->route('/:id_image') ->via('POST') ->to('#update') ->name('update_admin_product_image');
   $rrrr->route('/:id_image/delete') ->via('GET') ->to('#delete') ->name('delete_admin_product_image');
   $rrrr->route('/:id_image/edit') ->via('GET') ->to('#edit') ->name('edit_admin_product_image');
-
-  $rrrr = $rrr->route('/:id_product/variants') ->to('admin-products-variants#');
-  $rrrr->route('/') ->via('GET') ->to('#index') ->name('admin_product_variants');
-  $rrrr->route('/') ->via('POST') ->to('#create') ->name('create_admin_product_variant');
-  $rrrr->route('/new') ->via('GET') ->to('#new') ->name('new_admin_product_variant');
-  $rrrr->route('/:id_variant') ->via('GET') ->to('#show') ->name('admin_product_variant');
-  $rrrr->route('/:id_variant') ->via('POST') ->to('#update') ->name('update_admin_product_variant');
-  $rrrr->route('/:id_variant/delete') ->via('GET') ->to('#delete') ->name('delete_admin_product_variant');
-  $rrrr->route('/:id_variant/edit') ->via('GET') ->to('#edit') ->name('edit_admin_product_variant');
-
-
-  #$self->subresources($res, 'products');
-  #$self->resources('products');
-#
-# $r2 = $r->route('/admin')->to('admin#index')->name('admin#index');
-# $self->subresources($r2, 'products'),
-#
-# $self->resources(foo => {
-# 'bar' => '*',
-# 'bor' => {
-# except => ['show']
-# },
-# only => ['new','create']
-# });
 }
 
 sub setup_hooks {
@@ -112,12 +88,7 @@ sub setup_model {
     my $model = PerlEcommerce::Model->new(
         app => $self,
         root_schema => PerlEcommerce::Schema->connection(
-#dsn =>
-"dbi:$config->{type}:database=$config->{name};host=$config->{host};user=$config->{user};password=$config->{password}"
-#user => $config->{user},
-#password => $config->{password},
-#tabledefs => $self->config('database_tables'),
-#newquota => $self->config('new_quota_table'),
+          "dbi:$config->{type}:database=$config->{name};host=$config->{host};user=$config->{user};password=$config->{password}"
         )
     );
     $self->helper(model => sub { $model->model($_[1]) });
@@ -164,3 +135,99 @@ sub subresources {
 }
 
 1;
+
+=head1 NOM
+
+package I<PerlEcommerce> - module d'exemple commenté avec Pod
+
+=head1 SYNOPSIS
+
+Afin de lancer le serveur, vous devrez utiliser un script executable comme celui ci-dessous :
+
+	#!/usr/bin/env perl
+	use Mojo::Base -strict;
+	use File::Basename 'dirname';
+	use File::Spec;
+	use lib join '/', File::Spec->splitdir(dirname(__FILE__)), 'lib';
+	use lib join '/', File::Spec->splitdir(dirname(__FILE__)), '..', 'lib';
+	
+	# Application
+	$ENV{MOJO_APP} ||= 'PerlEcommerce';
+	
+	# Start commands
+	my $commands = Mojolicious::Commands->new;
+	push @{$commands->namespaces}, 'PerlECommerce::Command';
+	
+	Mojolicious::Commands->start_app($ENV{MOJO_APP}, 'daemon', '-l', 'http://*:8080');
+
+=head1 DESCRIPTION
+
+Ce module permet la mise en place d'un serveur proposant une application de ecommerce clés en main.
+
+La documentation est réalisée avec B<Pod>
+
+=head2 Fonctions
+
+=over 4
+
+=item startup()
+
+Permet de démarrer le serveur. Example: C<print PerlEcommerce::startup();>
+
+=item setup_plugins()
+
+Configure tous les plugins dont dépend l'application. Example: C<print PerlEcommerce::setup_plugins();>
+Pour ajouter un plugin, référez vous à la documentation du gestionnaire de plugins 
+L<Mojolicious::Plugins |http://mojolicio.us/perldoc/Mojolicious/Plugins>
+
+=item setup_routings()
+
+Définit toutes les routes de l'application. Example: C<print PerlEcommerce::setup_plugins();>
+Pour la définition des routes, référez vous à la documentation du gestionnaire de routes
+L<Mojolicious::Routes |http://mojolicio.us/perldoc/Mojolicious/Routes>
+
+=item setup_hooks()
+
+Définit les hooks de l'application.
+Pour la définition d'un hook, référez vous à la documentation:
+L<Mojolicious#hook |http://mojolicio.us/perldoc/Mojolicious#hook>
+
+=item setup_model()
+
+Instancie le modèle de données. Toutes les entités tu schema sont alors accessible via le 
+L<DBIx::Class::Schema::Loader |http://search.cpan.org/~genehack/DBIx-Class-Schema-Loader-0.07035/lib/DBIx/Class/Schema/Loader.pm>
+
+=item resources($controller_name) 
+
+Facilite la définition de routes en générant automatiquement les urls RESTful pour chacune 
+des méthodes CRUD et chacune des vues: index, show, edit. Example :
+
+	$self->resources('products');
+	
+	$self->resources(foo => {
+	    'bar' => '*',
+	    'bor' => {
+	    except => ['show']
+	  },
+	  only => ['new','create']
+	});
+
+=back
+
+=head1 AUTEUR
+
+Ghislain Loaec
+
+=head1 VOIR AUSSI
+
+L<PerlEcommerce::Model |PerlEcommerce::Model>
+
+L<PerlEcommerce::Controller |PerlEcommerce::Controller>
+
+L<PerlEcommerce::Schema |PerlEcommerce::Schema>
+
+L<PerlEcommerce::Command |PerlEcommerce::Command>
+
+L<PerlEcommerce::I18N |PerlEcommerce::I18N>
+
+=cut
