e1fda0e82f8e55fae1d3907169a4b0c055ad2328
KOSSAI Mustapha <kossai.mustapha@gmail.com>
2013-05-26 02:55:55 +0200

    MAKE SUR THAT the FORM IS FULL

--- public/js/lib/formToWizard.js
+++ public/js/lib/formToWizard.js
@@ -2,6 +2,7 @@
 
 (function($) {
     $.fn.formToWizard = function(options) {
+
         options = $.extend({  
             submitButton: "" 
         }, options); 
@@ -22,6 +23,7 @@
 
             // 2
             var name = $(this).find("legend").html();
+
             $("#steps").append("<li id='stepDesc" + i + "'>Step " + (i + 1) + "<span>" + name + "</span></li>");
 
             if (i == 0) {
@@ -52,10 +54,25 @@
         }
 
         function createNextButton(i) {
+
             var stepName = "step" + i;
             $("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Next' class='next'>Next ></a>");
 
             $("#" + stepName + "Next").bind("click", function(e) {
+                if($("#login").val()=="") {  alert('Login is required');return false;}
+                
+                if(!validateEmail($("#Email").val())) {  alert('Email is required or not valid');return false;}
+                if($("#Password").val()=="") {  alert('Password is required');return false;}
+                if(i==1){
+                    if($("#fname").val()=="") {  alert('FirstName is required');return false;}    
+                    if($("#lname").val()=="") {  alert('LastName is required');return false;}    
+                    if($("#ad1").val()=="") {  alert('Adress 1 is required');return false;}    
+                    if($("#City").val()=="") {  alert('City 1 is required');return false;}    
+                    if($("#phone").val()=="") {  alert('Phone Number 1 is required');return false;}    
+                    
+                }
+               
+
                 $("#" + stepName).hide();
                 $("#step" + (i + 1)).show();
                 if (i + 2 == count)
@@ -73,48 +90,12 @@
 })(jQuery); 
 
 
-function isValidCCNumber( ccNum ) {
-    var cardNum = new String( ccNum );
-    var digitsOnly = "";
-    // Filter out non-digit characters
-    for( var i = 0; i < cardNum.length; i++ ) {
-        if( "0123456789".indexOf( cardNum.charAt( i ) ) > -1 ) {
-            digitsOnly += cardNum.charAt( i );
-        }
-    }
-    // Perform Luhn check
-    var sum = 0;
-    var digit = 0;
-    var addend = 0;
-    var timesTwo = false;
-    for( var i = digitsOnly.length - 1; i >= 0; i-- ) {
-        digit = parseInt( digitsOnly.charAt( i ) );
-        if( timesTwo ) {
-            addend = digit * 2;
-            if( addend > 9 ) {
-                addend -= 9;
-            }
-        } else {
-            addend = digit;
-        }
-        sum += addend;
-        timesTwo = !timesTwo;
-    }
-    return sum % 10 == 0;
-}
-function checkLuhn(input) { 
-    var sum = 0;
-     var numdigits = input.length;
-      var parity = numdigits % 2;
-       for(var i=0; i < numdigits; i++) {
-        var digit = parseInt(input.charAt(i)); 
-        if(i % 2 == parity) 
-            digit *= 2; 
-            if(digit > 9) 
-                digit -= 9;
-                 sum += digit;
-     } return (sum % 10) == 0; 
-}
+ //  if(validateEmail($("#email").val())) {  alert('Email is required or not valid');return false;} else {return true;} 
+
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+} 
 function validate_creditcardnumber() {
     var numbers = $("#CardNumber").val();
     var regex = /(\d{4}-){3}\d{4}/,g
@@ -122,9 +103,15 @@ function validate_creditcardnumber() {
     return regex.test(numbers);
 }
 function formsubmit(){
-    if(validate_creditcardnumber()==true)
-    return true;
-    else return false;
+                    if($("#NameOnCard").val()=="") {  alert('NameOnCard is required');return false;}    
+                    if($("#CardNumber").val()=="") {  alert('CardNumber is required');return false;}    
+                    if($("#threenumbers").val()=="") {  alert('threenumbers 1 is required');return false;}    
+
+    if(validate_creditcardnumber()!=true)
+    {
+        alert("CARD NUMBER ISN'T VALID");
+        return false;
+    }
  // save input string and strip out non-numbers
    // if(!checkLuhn($("#CardNumber").val().replace(/[^\d]/g, ''))) { alert('Sorry, that is not a valid number - please try again!');return false; }    
   //  else return true;
